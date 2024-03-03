51a268a7a1663152b1a60be64e8b97e8374e2af0
sergioaf24 <71280922+sergioaf24@users.noreply.github.com>
2024-03-04 00:00:08 +0100

    Script procedural generation

--- Assets/Scripts/Minijuegos/2_Hambre0/ObjectInfo.cs
+++ Assets/Scripts/Minijuegos/2_Hambre0/ObjectInfo.cs
@@ -14,11 +14,16 @@ public class ObjectInfo : MonoBehaviour
     public float timeToCollect;
     public Material[] materials = new Material[3]; // Array of materials
     public GameObject Cultive;
-    
+    public MeshRenderer renderer;
+    public string Tipo;
+    private void Awake()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
 
     void Start()
     {
-        Renderer renderer = GetComponent<Renderer>();
+         
         miniGameManager = GameObject.Find("GameManager").GetComponent<MiniGameManager1>();
 
         switch (id)
@@ -37,22 +42,21 @@ public class ObjectInfo : MonoBehaviour
                 SetFruitStats("Lettuce", 5, 25);
                 timeLife = 10f;
                 renderer.material = materials[2];
+               
                 break;
             case 4:
                 SetFruitStats("Carrot", 8, 25);
                 timeLife = 7f;
                 break;
             case 5:
-                SetFruitStats("Spinach", 8, 25);
-                timeLife = 7f;
+                renderer.material = materials[0];
+                Debug.Log("Heloude");
                 break;
             case 6:
-                SetFruitStats("Broccoli", 10, 25);
-                timeLife = 7f;
+                renderer.material = materials[1];
                 break;
             case 7:
-                SetFruitStats("Onion", 10, 25);
-                timeLife = 5f;
+                renderer.material = materials[2];
                 break;
             case 8:
                 SetFruitStats("Pumpkin", 12, 30);
--- Assets/Scripts/Minijuegos/7_VidaDeEcosistemas/MiniGameManager7.cs
+++ Assets/Scripts/Minijuegos/7_VidaDeEcosistemas/MiniGameManager7.cs
@@ -4,21 +4,61 @@ using UnityEngine;
 
 public class MiniGameManager7 : MonoBehaviour
 {
-
-    public GameObject[] objectsToSpawn; 
-    public Vector3 spawnArea; 
+    public GameObject[] objectsToSpawn;
+    public Vector3 spawnArea;
     public int phasesProcess;
-    public GameObject[] ManageGameObjects ;
+    public GameObject[] ManageGameObjects;
     private int counterGameObject = 0;
     private int countItems;
-    public int countProgress=15;
-    public int currentPhase=1;
+    public int countProgress = 15;
+    public int currentPhase = 1;
+
+    private Vector3 randomPosition;
+
+    public float large;
+    public float hight;
+
+    private float xMaxZone1;
+    private float xMinZone1;
+    private float zMaxZone1;
+    private float zMinZone1;
+    public float zone1Large;
+    public float zone1Hight;
+
+    private float zMaxZone2;
+    private float zMinZone2;
+    private float xMaxZone2;
+    private float xMinZone2;
+
+    private float zMaxZone3;
+    private float zMinZone3;
+    private float xMaxZone3;
+    private float xMinZone3;
+
+    private bool readyToSpawn;
     void Start()
     {
-
+        GenerateArea();
         Phases(phasesProcess);
+        Debug.Log("large: " + large);
+        Debug.Log("hight: " + hight);
 
+        Debug.Log("xMaxZone1: " + xMaxZone1);
+        Debug.Log("xMinZone1: " + xMinZone1);
+        Debug.Log("zMaxZone1: " + zMaxZone1);
+        Debug.Log("zMinZone1: " + zMinZone1);
+        Debug.Log("zone1Large: " + zone1Large);
+        Debug.Log("zone1Hight: " + zone1Hight);
 
+        Debug.Log("zMaxZone2: " + zMaxZone2);
+        Debug.Log("zMinZone2: " + zMinZone2);
+        Debug.Log("xMaxZone2: " + xMaxZone2);
+        Debug.Log("xMinZone2: " + xMinZone2);
+
+        Debug.Log("zMaxZone3: " + zMaxZone3);
+        Debug.Log("zMinZone3: " + zMinZone3);
+        Debug.Log("xMaxZone3: " + xMaxZone3);
+        Debug.Log("xMinZone3: " + xMinZone3);
     }
 
     private void Update()
@@ -27,32 +67,177 @@ public class MiniGameManager7 : MonoBehaviour
         {
             phasesProcess = currentPhase;
             Phases(2);
-           
         }
     }
 
     void SpawnObject()
     {
-        // Genera una posición aleatoria dentro del área definida
-        Vector3 randomPosition = new Vector3(
-            transform.position.x + Random.Range(-spawnArea.x / 2, spawnArea.x / 2),
-            transform.position.y + 1,
-            transform.position.z + Random.Range(-spawnArea.z / 2, spawnArea.z / 2)
-        );
+        if (counterGameObject >= 0 && counterGameObject <= 10)
+        {
+            Vector3 areaToSpawn = GeneratePointToSpawn(1);
 
-        // Selecciona un objeto aleatorio del array
-        GameObject objectToSpawn = objectsToSpawn[Random.Range(0, objectsToSpawn.Length)];
+           
+                GameObject objectToSpawn = objectsToSpawn[0];
+                // Spawnear el objeto en la posición generada
+                Instantiate(objectToSpawn, areaToSpawn, Quaternion.identity);
 
-        // Spawnear el objeto en la posición generada
-        Instantiate(objectToSpawn, randomPosition, Quaternion.identity);
-        objectToSpawn.SetActive(true);
-        ManageGameObjects[counterGameObject] = objectToSpawn;
-        counterGameObject++;
+
+
+                ManageGameObjects[counterGameObject] = objectToSpawn;
+                objectToSpawn.GetComponent<ObjectInfo>().renderer.material = objectToSpawn.GetComponent<ObjectInfo>().materials[0];
+            objectToSpawn.GetComponent<ObjectInfo>().Tipo = "1";
+
+            Debug.Log(areaToSpawn.ToString());  
+            counterGameObject++;
+
+          
+        }
+        else if (counterGameObject > 10 && counterGameObject <= 20)
+        {
+            Vector3 areaToSpawn = GeneratePointToSpawn(2);
+            GameObject objectToSpawn = objectsToSpawn[Random.Range(0, objectsToSpawn.Length)];
+
+            if (readyToSpawn)
+            {
+
+
+                // Spawnear el objeto en la posición generada
+                Instantiate(objectToSpawn, areaToSpawn, Quaternion.identity);
+            objectToSpawn.GetComponent<ObjectInfo>().renderer.material = objectToSpawn.GetComponent<ObjectInfo>().materials[1];
+            ManageGameObjects[counterGameObject] = objectToSpawn;
+                objectToSpawn.GetComponent<ObjectInfo>().Tipo = "2";
+                counterGameObject++;
+            readyToSpawn = false;   
+
+            }
+            else
+            {
+                SpawnObject();
+            }
+        }
+        
+        else if (counterGameObject > 20 && counterGameObject <= 30)
+        {
+            readyToSpawn = true;
+            if (readyToSpawn)
+            {
+
+                Vector3 areaToSpawn = GeneratePointToSpawn(3);
+                GameObject objectToSpawn = objectsToSpawn[Random.Range(0, objectsToSpawn.Length)];
+
+                // Spawnear el objeto en la posición generada
+                Instantiate(objectToSpawn, areaToSpawn, Quaternion.identity);
+                objectToSpawn.GetComponent<ObjectInfo>().renderer.material = objectToSpawn.GetComponent<ObjectInfo>().materials[2];
+                ManageGameObjects[counterGameObject] = objectToSpawn;
+                objectToSpawn.GetComponent<ObjectInfo>().Tipo = "3";
+                counterGameObject++;
+                readyToSpawn = false;
+            }
+        else
+        {
+            SpawnObject();
+        }
+    }
     }
 
+    private Vector3 GeneratePointToSpawn(float zone)
+    {
+        Vector3 randomPosition = Vector3.zero;
+
+        switch (zone)
+        {
+            case 1:
+                randomPosition = new Vector3(
+                    Random.Range(xMinZone1, xMaxZone1),
+                    0f,
+                    Random.Range(zMinZone1, zMaxZone1)
+                );
+                break;
+
+            case 2:
+                for (int attemptsZone2 = 0; attemptsZone2 < 100; attemptsZone2++)
+                {
+                    randomPosition = new Vector3(
+                        Random.Range(xMinZone2, xMaxZone2),
+                        0f,
+                        Random.Range(zMinZone2, zMaxZone2)
+                    );
+
+                    if (randomPosition.x >= xMinZone1 && randomPosition.x <= xMaxZone1 &&
+                        randomPosition.z >= zMinZone1 && randomPosition.z <= zMaxZone1 && randomPosition.x >= xMinZone3)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        readyToSpawn = true;
+                        break;
+                    }
+                   
 
 
 
+
+
+                }
+                break;
+
+            case 3:
+                for (int attemptsZone3 = 0; attemptsZone3 < 100; attemptsZone3++)
+                {
+                    randomPosition = new Vector3(
+                        Random.Range(xMinZone3, xMaxZone3),
+                        0f,
+                        Random.Range(zMinZone3, zMaxZone3)
+                    );
+
+                    if (randomPosition.x >= xMinZone1 && randomPosition.x <= xMaxZone1 &&
+                        randomPosition.z >= zMinZone1 && randomPosition.z <= zMaxZone1 && randomPosition.x <= xMaxZone2)
+                    {
+                        continue; // Continuar con el siguiente intento si la posición está dentro de la zona 1
+                    }
+                    else
+                    {
+                        readyToSpawn = true;    
+
+                        break;
+                    }
+
+                  
+                    
+                }
+                break;
+        }
+
+        return randomPosition;
+    }
+
+    private void GenerateArea()
+    {
+        GameObject character = GameObject.Find("Character");
+        Vector3 posCharacter = character.transform.position;
+
+        hight = posCharacter.z + hight;
+
+        // Set points valid in zone 1
+        xMinZone1 = posCharacter.x - zone1Large;
+        xMaxZone1 = posCharacter.x + zone1Large;
+
+        zMinZone1 = (large / 2f) - zone1Hight;
+        zMaxZone1 = (large / 2f) + zone1Hight;
+
+        xMinZone2 = posCharacter.x - large;
+        xMaxZone2 = posCharacter.x;
+
+        zMinZone2 = posCharacter.z;
+        zMaxZone2 = posCharacter.z + hight;
+
+        xMinZone3 = posCharacter.x ;
+        xMaxZone3 = posCharacter.x + large;
+
+        zMinZone3 = posCharacter.z;
+        zMaxZone3 = posCharacter.z + hight;
+    }
     private void Phases(int phasesProcess)
     {
         switch (phasesProcess)
@@ -60,34 +245,26 @@ public class MiniGameManager7 : MonoBehaviour
             case 1:
                 for (int i = 0; i < 30; i++)
                 {
-                    SpawnObject();
+                    
+                   
+                        SpawnObject();
+                        
 
+                    
                 }
-
                 break;
             case 2:
-                
-                for(int x = 0; x < ManageGameObjects.Length; x++)
+                for (int x = 0; x < ManageGameObjects.Length; x++)
                 {
-
-                    if(ManageGameObjects[x].activeSelf==true)
+                    if (ManageGameObjects[x].activeSelf == true)
                     {
                         ManageGameObjects[x].SetActive(false);
                         Debug.Log(ManageGameObjects[x].name);
                     }
-                    
-                    
-                   
-                  
                 }
-
                 break;
-            
-         
             default:
-             
                 break;
         }
     }
-
 }
