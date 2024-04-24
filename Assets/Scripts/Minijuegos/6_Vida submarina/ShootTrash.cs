e394e4a52c4343a738fe3af53a1c389f5386ad6a
Pablo Villar <tracermc14@gmail.com>
2024-04-24 20:34:39 +0200

    Water and animations underwater changed, grow shader added, underwater effect for the submarine life minigame and feedback programmed for submarine life minigame

--- Assets/Scripts/Minijuegos/6_Vida submarina/ShootTrash.cs
+++ Assets/Scripts/Minijuegos/6_Vida submarina/ShootTrash.cs
@@ -9,10 +9,12 @@ public class ShootTrash : MonoBehaviour
     public int points;
     public Camera playerCamera;
 
+    public GameObject trashFeedbackPanel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        trashFeedbackPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -34,6 +36,7 @@ public class ShootTrash : MonoBehaviour
             Debug.DrawLine(transform.position, hit.point, Color.yellow);
             hit.transform.gameObject.transform.position = relocationPosition; //+ new Vector3(Random.Range(10f, 20f),0,0) ;
             relocationPosition += new Vector3(2f, 0, 0);
+            StartCoroutine(ShowTrashFeedbackPanel());
             points++;
         }
     }
@@ -41,5 +44,12 @@ public class ShootTrash : MonoBehaviour
     private void OnDrawGizmos()
     {
         Debug.DrawRay(transform.GetChild(0).transform.rotation.eulerAngles, transform.GetChild(0).transform.forward * Mathf.Infinity, Color.yellow);
+    }
+
+    private IEnumerator ShowTrashFeedbackPanel()
+    {
+        trashFeedbackPanel.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        trashFeedbackPanel.SetActive(false);
     }
 }
